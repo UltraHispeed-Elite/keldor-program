10c2bb37a0d3e09eb07353fb7748c5a474cdaf7f
UltraHispeed-Elite <62296749+UltraHispeed-Elite@users.noreply.github.com>
2024-04-04 09:40:27 -0700

    dump

--- sketch.js
+++ sketch.js
@@ -4,6 +4,11 @@ var player;
 
 var steps;
 var followDirection;
+var scores;
+var didScore
+var average;
+var ranking1;
+var ranking2;
 
 function setup() {
     let screen = createCanvas(400,400);
@@ -21,26 +26,47 @@ function setup() {
     followDirection = [];
     followDirection.length = iteration_count;
 
+    scores = [];
+    scores.length = iteration_count;
+
+    didScore = [];
+    didScore.length = iteration_count;
+
+    average = [];
+    average.length = iteration_count;
+
+    ranking1 = [];
+    ranking1.length = iteration_count;
+
+    ranking2 = [];
+    ranking2.length = iteration_count;
+
     for(let i=0; i<iteration_count; i++) {
         new player.Sprite(200,200);
 
         steps[i] = 0;
-    }
 
-    localStorage.clear();
+        scores[i] = 0;
+
+        didScore[i] = false;
+
+        localStorage.removeItem("path"+(i+1));
+    }
 }
 
 function draw() {
     background("black");
 
     if(frameCount % 250 === 0) {
-        //clear();
+        clear();
     }
 
-    if(steps[0] < 10) {
+    if(steps[0] < 100) {
         for(let i = 0; i<iteration_count; i++) {
-            if(frameCount % 20 === 0) {
+            if(frameCount % 5 === 0) {
                 moveKeldor(i);
+                scoreKeldor(i);
+                rankKeldor(i);
                 saveKeldor(i);
             }
         }
@@ -66,9 +92,46 @@ function moveKeldor(iteration) {
     }
 
     move(iteration, followDirection[iteration]);
-
-    console.log(followDirection[iteration]);
 }
+
+function scoreKeldor(iteration) {
+    let shouldScore = steps[iteration];
+    let desiredDirection;
+    if(shouldScore % 4 === 0) {
+        desiredDirection = "up";
+    }else if(shouldScore % 3 === 0) {
+        desiredDirection = "left";
+    }else if(shouldScore % 2 === 0) {
+        desiredDirection = "down";
+    }else if(shouldScore % 1 === 0) {
+        desiredDirection = "right";
+    }
+
+    if(desiredDirection === followDirection[iteration]) {
+        scores[iteration] += 1;
+        didScore[iteration] = true;
+    }else {
+        didScore[iteration] = false;
+    }
+
+    average[iteration] = scores[iteration]/steps[iteration];
+}
+
+function rankKeldor(iteration) {
+    ranking1[iteration] = average[iteration];
+
+    ranking1 = ranking1.sort(function (a, b) {  return a - b; });
+
+    for(let i = 0; i<iteration_count; i++) {
+        if(ranking1[i] === average[iteration]) {
+            ranking2[iteration] = (i+1);
+        }
+    }
+
+    console.log("iteration "+(iteration+1)+" is rank "+ ranking2[iteration]);
+
+}
+
 
 function saveKeldor(iteration) {
     let find_path = localStorage.getItem("path"+(iteration+1));
@@ -85,6 +148,10 @@ function saveKeldor(iteration) {
     let score = {
         "steps": steps[iteration],
         "direction": followDirection[iteration],
+        "score": scores[iteration],
+        "didScore": didScore[iteration],
+        "score/step": average[iteration],
+        "ranking": ranking2[iteration],
         "position": {
             "x": player[iteration].x,
             "y": player[iteration].y
@@ -92,9 +159,13 @@ function saveKeldor(iteration) {
     }
 
     path["path"].push(score);
+    
+    let exportPath = JSON.stringify(path);
 
-    localStorage.setItem("path"+(iteration+1), JSON.stringify(path));
+    localStorage.setItem("path"+(iteration+1), exportPath);
 }
+
+
 
 function move(iteration,direction) {
     if(direction === "up") {
